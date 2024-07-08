47eafa7c3fda006a2ee9ec1857a47daa4fdc889e
Umair <umairshah0711@gmail.com>
2024-07-08 22:57:32 +0530

    final changes 1.1.0

--- src/components/ProductComponent.jsx
+++ src/components/ProductComponent.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
-import image1 from './../assets/Product1.jpg';
-import image2 from './../assets/Picture2.jpg';
-import image3 from './../assets/Picture3.jpg';
-import image4 from './../assets/Picture4.jpg';
-import image5 from './../assets/Picture5.jpg';
-import image6 from './../assets/Picture6.jpg';
-import image7 from './../assets/Picture7.jpg';
-import image8 from './../assets/Picture8.jpg';
-
+import image1 from "./../assets/Product1.jpg";
+import image2 from "./../assets/Picture2.jpg";
+import image3 from "./../assets/Picture3.jpg";
+import image4 from "./../assets/Picture4.jpg";
+import image5 from "./../assets/Picture5.jpg";
+import image6 from "./../assets/Picture6.jpg";
+import image7 from "./../assets/Picture7.jpg";
+import image8 from "./../assets/Picture8.jpg";
 
 const ProductComponent = () => {
   const products = [
@@ -32,7 +31,7 @@ const ProductComponent = () => {
     {
       name: "Drying Tumbler",
       capacityrange: "Available in 10-125 kg capacity.",
-      function: "Choice of control panel manual, microprocessor and PLC.",
+      function: "Choice of control panel manual, microprocessor, PLC.",
       function1:
         "Heating System Gas, Steam, Electric heating and Thermic fluid.",
       function2: "Unique air flow ensures maximum heat utilization.",
@@ -42,7 +41,7 @@ const ProductComponent = () => {
       name: "Side Loading Washing Machine",
       capacityrange: "Available in 100-400kg capacity double door system.",
       function: "Efficient garment/Knitwear processor and washer.",
-      function1: "Choice of control panel manual, microprocessor and PLC.",
+      function1: "Choice of control panel manual, microprocessor, PLC.",
       function2:
         "Carries out Silicon wash, softening, bleaching, stoning, dyeing, enzyme and many more.",
       function3: "Choice of drive gear box/sprocket.",
@@ -52,8 +51,8 @@ const ProductComponent = () => {
       name: "Dyeing Front Loading Washing Machine",
       capacityrange: "Available in 100-480kg capacity.",
       function:
-        "Efficient garment/Knitwear processor, washing and also O-zonator machine.",
-      function1: "Choice of control panel manual, microprocessor and PLC.",
+        "Efficient garment/Knitwear processor, washing , O-zonator machine.",
+      function1: "Choice of control panel manual, microprocessor, PLC.",
       function2:
         "Carries out Silicon wash, softening, bleaching, stoning, dyeing, enzyme and many more.",
       function3: "Choice of drive gear box/timing pulley.",
@@ -63,7 +62,7 @@ const ProductComponent = () => {
     {
       name: "Drying Tumbler Jumbo Forward Tilting",
       capacityrange: "Available in 150,200 and 250 kg capacity.",
-      function: "Choice of control panel manual, microprocessor & PLC.",
+      function: "Choice of control panel manual, microprocessor, PLC.",
       function1:
         "Heating System Gas, Steam, Electric heating and Thermic fluid.",
       function2: "Dual radiator with Top to bottom air flow.",
@@ -80,11 +79,20 @@ const ProductComponent = () => {
     },
     {
       name: "Curing Ovens",
-      capacityrange: "5 pants, 30 pants, 60 pants.",
+      capacityrange: "Availabe in 5 pants, 30 pants, 60 pants.",
       function: "Batch type and also available in Conveyor.",
       function1: "Heating system, Electric heating or gas fired.",
       function2: "Trolley System or Conveyor System.",
       image: image8,
+    },
+    {
+      name: "Hydro Extractor(Centrifuge)",
+      capacityrange: "Availabe in 25, 50, 75 and 100kg.",
+      function: "Heavy dusty 3 points suspension type high speed centrifuge",
+      function1: "Stainless steel inner & outer drum.",
+      function2: "Inverter driver for soft start stop & breaking register.",
+      function3: "Centrifugal cluth & gradual accelaration.",
+      image: "/assets/HEC.jpg",
     },
   ];
 
